bb842817f8362e5d073b608df9d88deeef1edb91
Vladislav <cyberanonym2047@gmail.com>
2026-01-12 00:07:54 +0300

    done

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def visit(self, url: str):
+        self.page.goto(url, wait_until='networkidle')
+
+    def reload(self):  # Метод для перезагрузки страницы
+        self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
---
+++ tools/allure/severity.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Severity(str, Enum):
+    BLOCKER = 'blocker'
+    CRITICAL = 'critical'
+    NORMAL = 'normal'
+    MINOR = 'minor'
+    TRIVIAL = 'trivial'
